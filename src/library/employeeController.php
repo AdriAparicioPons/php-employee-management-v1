b39c753b798a8582ec6e7b9b16ec9b9cbff6238e
Adrià Aparicio Pons <adriapariciopons@gmail.com>
2022-06-05 18:05:51 +0200

    add new Employee

--- src/library/employeeController.php
+++ src/library/employeeController.php
@@ -2,3 +2,6 @@
 
 require_once "employeeManager.php";
 dataEmployees();
+if (isset($_POST['submit'])){
+    addEmployee();
+}
--- src/library/employeeManager.php
+++ src/library/employeeManager.php
@@ -1,5 +1,39 @@
 <?php
 
+
+
+function addEmployee() {
+   $currentEmployees = file_get_contents("../../resources/employees.json");
+    $employeeArray = json_decode($currentEmployees, true);
+    $new_employee = 
+    [
+       // "id"=> que sume 1  al anterior
+       // img = > random
+        "name" =>  $_POST['firstName'],
+        "lastName" => $_POST['lastName'],
+        "email" => $_POST['email'],
+        "gender" => $_POST['gender'],
+        "city" =>  $_POST['city'],
+        "streetAddress" => $_POST['address'],
+        "state" => $_POST['state'],
+        "age" =>  $_POST['age'],
+        "postalCode" => $_POST['zip'],
+        "phoneNumber" =>$_POST['phone']
+    ];
+    
+    $employeeArray [] = $new_employee;
+    
+    
+    $finalEmployeeArray = json_encode($employeeArray, JSON_PRETTY_PRINT);
+   
+    file_put_contents('../../resources/employees.json', $finalEmployeeArray);
+ 
+    
+}
+
+
+
+
 function dataEmployees(){
     $pathEmployees = "../../resources/employees.json";
     $dataEmployees = json_decode(file_get_contents($pathEmployees));
@@ -29,67 +63,9 @@ function dataEmployees(){
             echo "<td>$userPhoneNumberEmployees</td>";
             echo "</tr>";
             echo "</table>";
-        }
+        } 
     }
 }
 
-// echo "<table>";
-//         echo "<tr>";
-//         echo "<td>$userNameEmployees</td>";
-//         echo "<td>$userLastNameEmployees</td>";
-//         echo "<td>$userEmailEmployees</td>";
-//         echo "<td>$userGenderEmployees</td>";
-//         echo "<td>$userCityEmployees</td>";
-//         echo "<td>$userAddressEmployees</td>";
-//         echo "<td>$userStateEmployees</td>";
-//         echo "<td>$userAgeEmployees</td>";
-//         echo "<td>$userPostalEmployees</td>";
-//         echo "<td>$userPhoneNumberEmployees</td>";
-//         echo "</tr>";
-//         echo "</table>";
-
-function printEmployees(){
-
-}
-
-dataEmployees();
-
-function addEmployee(array $newEmployee)
-{
-// TODO implement it
-}
 
 
-function deleteEmployee(string $id)
-{
-// TODO implement it
-}
-
-
-function updateEmployee(array $updateEmployee)
-{
-// TODO implement it
-}
-
-
-function getEmployee(string $id)
-{
-// TODO implement it
-}
-
-
-function removeAvatar($id)
-{
-// TODO implement it
-}
-
-
-function getQueryStringParameters(): array
-{
-// TODO implement it
-}
-
-function getNextIdentifier(array $employeesCollection): int
-{
-// TODO implement it
-}
